51b60c78abfe84d199ee5fba39fa752609a7fc18
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-07 15:17:32 +0200

    Wrapped the Machine Learning Page with the Grid

--- src/pages/MachineLearning/MachineLearning.js
+++ src/pages/MachineLearning/MachineLearning.js
@@ -1,8 +1,10 @@
+import { Grid } from "@mui/material";
+
 function MachineLearning() {
     return (
-        <div>
+        <Grid item xs={8}>
             This is the Machine Learning Page.
-        </div>
+        </Grid>
     )
 }
 
