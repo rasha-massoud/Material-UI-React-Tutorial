a686d072a08f82aa72214aca2137267d051364da
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-07 15:18:13 +0200

    Wrapped the Storage Page with the Grid

--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,8 +1,10 @@
+import { Grid } from "@mui/material";
+
 function Storage() {
     return (
-        <div>
+        <Grid item xs={8}>
             This is the Storage Page.
-        </div>
+        </Grid>
     )
 }
 
