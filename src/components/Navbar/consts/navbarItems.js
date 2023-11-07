904aa50c7145bb34baf6a02ef025567d69ffc124
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-07 11:38:21 +0200

    Created the Navbar items constants file

---
+++ src/components/Navbar/consts/navbarItems.js
@@ -0,0 +1,38 @@
+export const mainNavbarItems = [
+    {
+        id: 0,
+        icon: "",
+        label: "Authentication",
+        route: "",
+    },
+    {
+        id: 1,
+        icon: "",
+        label: "Database",
+        route: "",
+    },
+    {
+        id: 2,
+        icon: "",
+        label: "Storage",
+        route: "",
+    },
+    {
+        id: 3,
+        icon: "",
+        label: "Hosting",
+        route: "",
+    },
+    {
+        id: 4,
+        icon: "",
+        label: "Functions",
+        route: "",
+    },
+    {
+        id: 5,
+        icon: "",
+        label: "Machine Learning",
+        route: "",
+    }
+]
