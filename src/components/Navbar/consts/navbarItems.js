3621138a1f28c594e53d2716a31330979c1e072f
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-07 11:57:19 +0200

    Added the Icons to the navbar items constants

--- src/components/Navbar/consts/navbarItems.js
+++ src/components/Navbar/consts/navbarItems.js
@@ -1,37 +1,44 @@
+import PeopleIcon from '@mui/icons-material/People';
+import DnsIcon from '@mui/icons-material/Dns';
+import ImageIcon from '@mui/icons-material/Image';
+import PublicIcon from '@mui/icons-material/Public';
+import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+import SettingsInputComponentIcon from '@mui/icons-material/SettingsInputComponent';
+
 export const mainNavbarItems = [
     {
         id: 0,
-        icon: "",
+        icon: <PeopleIcon />,
         label: "Authentication",
         route: "",
     },
     {
         id: 1,
-        icon: "",
+        icon: <DnsIcon />,
         label: "Database",
         route: "",
     },
     {
         id: 2,
-        icon: "",
+        icon: <ImageIcon />,
         label: "Storage",
         route: "",
     },
     {
         id: 3,
-        icon: "",
+        icon: <PublicIcon />,
         label: "Hosting",
         route: "",
     },
     {
         id: 4,
-        icon: "",
+        icon: <SettingsEthernetIcon />,
         label: "Functions",
         route: "",
     },
     {
         id: 5,
-        icon: "",
+        icon: <SettingsInputComponentIcon />,
         label: "Machine Learning",
         route: "",
     }
