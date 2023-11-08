b551bac1f52bec3f5d181b74243fbf66644675a7
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-08 22:32:21 +0200

    Updated the Header Compoennt

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,39 @@
 import CommonButton from '../../components/CommonButton/CommonButton';
 import NotificationBell from "../../components/NotificationBell/NotificationBell";
 import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import HelpIcon from '@mui/icons-material/Help';
 
-const Header = () => {
+const Header = ({ title }) => {
     return (
         <div>
-            <CommonButton variant='default'>
-                Go to docs
-            </CommonButton>
+            <div>
+                <CommonButton variant='default'>
+                    Go to docs
+                </CommonButton>
 
-            <NotificationBell 
-                iconColor='primary' 
-            />
+                <NotificationBell 
+                    iconColor='primary' 
+                />
 
-            <Avatar 
-                src='https://mui.com/static/images/avatar/1.jpg' 
-                alt='Profile Picture'
-            />
+                <Avatar 
+                    src='https://mui.com/static/images/avatar/1.jpg' 
+                    alt='Profile Picture'
+                />
+            </div>
+
+            <div>
+                <Typography>{title}</Typography>
+                <CommonButton variant='outlined'>Web Setup</CommonButton>
+                
+                <Tooltip title='Help'>
+                    <IconButton>
+                        <HelpIcon />
+                    </IconButton>
+                </Tooltip>
+            </div>
         </div>
     )
 }
