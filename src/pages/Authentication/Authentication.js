5440558e5bffcf293ddce31e28cb9e7c51f7a9c5
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-08 19:31:56 +0200

    Imported the NotificationBell Component to the Authentication Page

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,11 +1,14 @@
 import { Grid } from "@mui/material";
-import CommonButton from '../../components/CommonButton/CommonButton';
 import { buttonStyles } from "./styles";
+import CommonButton from '../../components/CommonButton/CommonButton';
+import NotificationBell from "../../components/NotificationBell/NotificationBell";
 
 const Authentication = () => {
 
     return (
         <Grid item xs={8}>
+
+            <NotificationBell iconColor='primary' badgeContent='4'/>
             This is the Authentication Page.
             <CommonButton 
                 variant='outlined'
