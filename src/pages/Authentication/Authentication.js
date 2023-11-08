cee6eeb67c0115401329bb2183a585511803011e
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-08 22:19:10 +0200

    Added the Avatar tag tot he Authentication Page

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -2,6 +2,7 @@ import { Grid } from "@mui/material";
 import { buttonStyles } from "./styles";
 import CommonButton from '../../components/CommonButton/CommonButton';
 import NotificationBell from "../../components/NotificationBell/NotificationBell";
+import Avatar from '@mui/material/Avatar';
 
 const Authentication = () => {
 
@@ -9,11 +10,19 @@ const Authentication = () => {
         <Grid item xs={8}>
             This is the Authentication Page.
 
+            <CommonButton variant='default'>
+                Go to docs
+            </CommonButton>
+
             <NotificationBell 
                 iconColor='primary' 
             />
 
-
+            <Avatar 
+                src='https://mui.com/static/images/avatar/1.jpg' 
+                alt='Profile Picture'
+            />
+            
             <CommonButton 
                 variant='outlined'
                 sx={buttonStyles}
