3a4cceae330eeeda315a26fd8daf9805a444cd38
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-08 19:29:33 +0200

    Updated the NotificationBell Component

--- src/components/NotificationBell/NotificationBell.js
+++ src/components/NotificationBell/NotificationBell.js
@@ -1,14 +1,18 @@
 import Badge from '@mui/material/Badge';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 
 const NotificationBell = ({ iconColor, badgeContent }) => {
     return (
-        <IconButton color={iconColor} aria-label={notificationsLabel(100)}>
-            <Badge badgeContent={badgeContent} color="primary">
-                <NotificationsIcon color="action" />
-            </Badge>
-        </IconButton>
+        <Tooltip title={`You have ${badgeContent} new notifications!`}>
+            <IconButton color={iconColor}>
+                <Badge badgeContent={badgeContent} color="primary">
+                    <NotificationsIcon />
+                </Badge>
+            </IconButton>
+        </Tooltip>
+
     )
 }
 
