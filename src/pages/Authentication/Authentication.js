e9b67eb867ad9910d316fe54841d86bb53c4af85
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-08 12:36:19 +0200

    Added the CommonButton Component to the Authentication page

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,9 +1,43 @@
 import { Grid } from "@mui/material";
+import CommonButton from '../../components/CommonButton/CommonButton';
 
 const Authentication = () => {
+    const buttonStyles = {
+        fontWeight: 600,
+        fontSize: '0.875rem',
+        textTransform: 'capitalize',
+        borderRadius: 2.5,
+        '&.MuiButton-contained': {
+            backgroundColor: '#009be5',
+            '&:hover': {
+                backgroundColor: '#006db3',
+            }
+        },
+        '&.MuiButton-outlined': {
+            color: '#fff',
+            borderColor: '#fff',
+            '&:hover': {
+                backgroundColor: 'transparent',
+            }
+        },
+
+    }
+
     return (
         <Grid item xs={8}>
             This is the Authentication Page.
+            <CommonButton 
+                variant='outlined'
+                sx={buttonStyles}
+            >
+                Web Setup 
+            </CommonButton>
+            <CommonButton 
+                variant='contained'
+                sx={buttonStyles}
+            >
+                Add User 
+            </CommonButton>
         </Grid>
     )
 }
