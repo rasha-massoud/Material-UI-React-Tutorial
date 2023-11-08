314c52c1c19827a330cf482b1eccb01ab20f09d8
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-08 20:30:01 +0200

    Updated the authentication Page to include the BasicMenu Compoennt

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import { Grid } from "@mui/material";
 import { buttonStyles } from "./styles";
 import CommonButton from '../../components/CommonButton/CommonButton';
 import NotificationBell from "../../components/NotificationBell/NotificationBell";
+import BasicMenu from "../../components/BasicMenu/BasicMenu";
 
 const Authentication = () => {
+    const [open, setOpen] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const [menuItems, setMenuItems] = useState([]);
+
+    const handleOpen = (event) => {
+        setAnchorEl(event.currentTarget)
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
 
     return (
         <Grid item xs={8}>
-
-            <NotificationBell iconColor='primary' badgeContent={0}/>
             This is the Authentication Page.
+
+            <NotificationBell 
+                iconColor='primary' 
+                badgeContent={0}
+                anchorEl={anchorEl}
+                onClick={handleOpen}
+            />
+            <BasicMenu 
+                open={open}
+                anchorEl={anchorEl}
+                handleClose={handleClose}
+                menuItems={menuItems}
+            />
+
             <CommonButton 
                 variant='outlined'
                 sx={buttonStyles}
