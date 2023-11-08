8e07b40eaa198d7c4584567b60acb9c36214443c
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-08 22:10:51 +0200

    Updated the NotificationBell Compoennt to include the BasicMenu

--- src/components/NotificationBell/NotificationBell.js
+++ src/components/NotificationBell/NotificationBell.js
@@ -1,25 +1,58 @@
+import { useState } from 'react';
 import Badge from '@mui/material/Badge';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
+import BasicMenu from '../../components/BasicMenu/BasicMenu';
 
-const NotificationBell = ({ iconColor, badgeContent, anchorEl, onClick }) => {
-    const newNotifications = `You have ${badgeContent} new notifications!`;
+const NotificationBell = ({ iconColor }) => {
+    
+    const [open, setOpen] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+    
+    const notifications = [
+        {
+            id: 0,
+            label: 'First Notification'
+        },
+        {
+            id: 1,
+            label: 'Second Notification'
+        },
+    ];
+    
+    const newNotifications = `You have ${notifications.length} new notifications!`;
     const noNotifications = 'No new notifications';
     
-    return (
-        <Tooltip title={ badgeContent ? newNotifications : noNotifications }>
-            <IconButton 
-                color={iconColor}
-                onClick={onClick}
-                anchorEl={anchorEl}
-            >
-                <Badge badgeContent={badgeContent} color='primary'>
-                    <NotificationsIcon />
-                </Badge>
-            </IconButton>
-        </Tooltip>
+    const handleOpen = (event) => {
+        setAnchorEl(event.currentTarget)
+        setOpen(true)
+    };
 
+    const handleClose = () => {
+        setOpen(false)
+    };
+
+    return (
+        <>
+            <Tooltip title={ notifications.length ? newNotifications : noNotifications }>
+                <IconButton 
+                    color={iconColor}
+                    onClick={ notifications.length ? handleOpen : null }
+                    anchorEl={anchorEl}
+                >
+                    <Badge badgeContent={notifications.length} color='primary'>
+                        <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+            </Tooltip>
+            <BasicMenu 
+                open={open}
+                anchorEl={anchorEl}
+                handleClose={handleClose}
+                menuItems={notifications}
+            />
+        </>
     )
 }
 
