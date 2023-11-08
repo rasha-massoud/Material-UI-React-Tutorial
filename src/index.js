c7053b0c97d9a07cd79a41b37b4ec0a4c1e5c05f
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-08 12:38:18 +0200

    Imported and wrapped the ThemeProvider to the index.js file

--- src/index.js
+++ src/index.js
@@ -10,21 +10,24 @@ import Functions from './pages/Functions/Functions';
 import Hosting from './pages/Hosting/Hosting';
 import MachineLearning from './pages/MachineLearning/MachineLearning';
 import Storage from './pages/Storage/Storage';
+import { ThemeProvider } from '@emotion/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} >
-        <Route path="authentication" element={<Authentication />} />
-        <Route path="database" element={<Database />} />
-        <Route path="functions" element={<Functions />} />
-        <Route path="hosting" element={<Hosting />} />
-        <Route path="machine-learning" element={<MachineLearning />} />
-        <Route path="storage" element={<Storage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="authentication" element={<Authentication />} />
+          <Route path="database" element={<Database />} />
+          <Route path="functions" element={<Functions />} />
+          <Route path="hosting" element={<Hosting />} />
+          <Route path="machine-learning" element={<MachineLearning />} />
+          <Route path="storage" element={<Storage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
