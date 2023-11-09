e3dd7fe04744eacb00f31223e52f9c5c19ea03d1
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-09 14:51:33 +0200

    Imported the Header Component to the Database Page

--- src/pages/Database/Database.js
+++ src/pages/Database/Database.js
@@ -1,6 +1,10 @@
+import Header from "../../components/Header/Header";
+
 function Database() {
     return (
         <div style={{ marginLeft:320}}>
+            <Header title='Database'/>
+
             This is the Database Page.
         </div>
     )
