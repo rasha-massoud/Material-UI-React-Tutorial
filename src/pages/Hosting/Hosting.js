fa73bc3a9cbe098859ed114c540d776e2ce5d040
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-09 14:53:41 +0200

    Imported the Header Component to the Hosting Page

--- src/pages/Hosting/Hosting.js
+++ src/pages/Hosting/Hosting.js
@@ -1,6 +1,10 @@
+import Header from "../../components/Header/Header";
+
 function Hosting() {
     return (
         <div style={{ marginLeft:320}}>
+            <Header title='Hosting'/>
+
             This is the Hosting Page.
         </div>
     )
