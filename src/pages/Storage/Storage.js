2e80728b07acf75384cc5554d8387a0c5b5107b8
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-09 14:55:06 +0200

    Imported the Header Component to the Storage Page

--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,6 +1,10 @@
+import Header from "../../components/Header/Header";
+
 function Storage() {
     return (
         <div style={{ marginLeft:320}}>
+            <Header title='Storage'/>
+
             This is the Storage Page.
         </div>
     )
