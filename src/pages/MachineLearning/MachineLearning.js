f520494fe9baa7242218c88fad0bde0d8dbcb9af
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-09 14:54:24 +0200

    Imported the Header Component to the Machine Learning Page

--- src/pages/MachineLearning/MachineLearning.js
+++ src/pages/MachineLearning/MachineLearning.js
@@ -1,6 +1,10 @@
+import Header from "../../components/Header/Header";
+
 function MachineLearning() {
     return (
         <div style={{ marginLeft:320}}>
+            <Header title='Machine Learning'/>
+
             This is the Machine Learning Page.
         </div>
     )
