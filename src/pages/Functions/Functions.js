98307cf463548224d4884bea479cc3515f7fd542
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-09 14:52:23 +0200

    Imported the Header Component to the Functions Page

--- src/pages/Functions/Functions.js
+++ src/pages/Functions/Functions.js
@@ -1,6 +1,10 @@
+import Header from "../../components/Header/Header";
+
 function Functions() {
     return (
         <div style={{ marginLeft:320}}>
+            <Header title='Functions'/>
+
             This is the Functions Page.
         </div>
     )
