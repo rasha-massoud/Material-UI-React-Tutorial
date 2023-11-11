13d096434d5326845e7ff8bd2140be3aec304012
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-11 11:24:05 +0200

    Created the SearchBar Compoennt

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,31 @@
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import Input from '@mui/material/Input';
+import CommonButton from '../CommonButton/CommonButton';
+import { buttonStyles } from '../../pages/Authentication/styles';
+
+const SearchBar = ({ placeholder, onChange }) => {
+    return (
+        <div>
+            <IconButton >
+                <SearchIcon />
+            </IconButton>
+            <Input 
+                paceholder={placeholder} 
+                onChange={onChange}
+            />
+            <CommonButton 
+                variant='contained'
+                sx={buttonStyles}
+            >
+                Add User 
+            </CommonButton>
+            <IconButton >
+                <RefreshIcon />
+            </IconButton>
+        </div>
+    )
+}
+
+export default SearchBar;
