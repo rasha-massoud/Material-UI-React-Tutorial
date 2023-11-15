058f752041879379b0b402ce88bb01bc50a02aba
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-15 16:25:15 +0200

    Added the BasicModal component to the Authentication Page

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,13 +1,25 @@
 import Header from "../../components/Header/Header";
 import BasicCard from "../../components/BasicCard/BasicCard";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import BasicModal from "../../components/BasicModal/BasicModal";
+import { useState } from "react";
 
 const Authentication = () => {
 
+    const [open,setOpen] = useState(false);
+    
     const handleChange = (value) => {
         console.log(value);
     }
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
     
+    const handleClose = () => {
+        setOpen(false);
+    }
+
     return (
         <div style={{ marginLeft:320}}>
             <Header title='Authentication'/>
@@ -16,10 +28,16 @@ const Authentication = () => {
                     <SearchBar 
                         placeholder={'Search by email address, phone number, or user ID'} 
                         onChange={(event) => handleChange(event.target.value)}
+                        onClick={handleOpen}
                     />
                 }
                 content={'No users for this project yet.'}
             />
+
+            
+            <BasicModal open={open} onClose={handleClose} />               
+
+            
         </div>
     )
 }
