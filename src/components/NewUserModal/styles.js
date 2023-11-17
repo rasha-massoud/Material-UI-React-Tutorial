284202bc647549ff7dd96f42f57de64c507dc398
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-17 11:36:55 +0200

    Added the styles file of the NewUserModal Component

---
+++ src/components/NewUserModal/styles.js
@@ -0,0 +1,11 @@
+export const newUserModalStyles = {
+    inputFields: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '20px',
+        marginBottom: '15px',
+        '.MuiFormControl-root': {
+            marginBottom: '20px',
+        }
+    }
+}
