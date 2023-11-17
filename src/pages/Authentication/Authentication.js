df6df71f78f3d8badfc56a312545fcdfd04d5fe5
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-17 11:59:55 +0200

    Updated and Completed the functionality of the SearchBar in the Authentication Page

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -10,10 +10,24 @@ const Authentication = () => {
 
     const [open,setOpen] = useState(false);
     const [users, setUsers] = useState([]);
+    const [searchResults, setSearchResults] = useState(users);
 
-    const handleChange = (value) => {
-        console.log(value);
-    }
+    const handleSearch = (value) => {
+        filterData(value)
+    };
+
+    const filterData = (value) => {
+        const lowercasedValue = value.toLowerCase().trim();
+        if (lowercasedValue === '') setUsers(searchResults);
+        else {
+            const filteredData = searchResults.filter((item) => {
+                return Object.keys(item).some((key) => 
+                item[key].toString().toLowerCase().includes(lowercasedValue)
+                );
+            });
+            setUsers(filteredData)
+        };
+    };
 
     const addNewUser = (data) => {
         users.push({ ...data });
@@ -22,11 +36,11 @@ const Authentication = () => {
 
     const handleOpen = () => {
         setOpen(true);
-    }
+    };
     
     const handleClose = () => {
         setOpen(false);
-    }
+    };
 
     return (
         <div style={{ marginLeft:320}}>
@@ -35,14 +49,14 @@ const Authentication = () => {
                 header = {
                     <SearchBar 
                         placeholder={'Search by email address, phone number, or user ID'} 
-                        onChange={(event) => handleChange(event.target.value)}
+                        onChange={(event) => handleSearch(event.target.value)}
                         onClick={handleOpen}
                     />
                 }
                 content = {
                     users.length ? (
                         users.map((user) => (
-                            <Box sx={{ marginBottom: '20px' }}>
+                            <Box sx={{ marginBottom: '20px' }} key={user.id}>
                                 <Typography>User ID: {user.userId}</Typography>
                                 <Typography>Email: {user.email}</Typography>
                                 <Typography>Phone Number: {user.phoneNumber}</Typography>
