afb304169397912b13502156694de7c4959ffb09
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-18 21:45:24 +0200

    Updated the Database Page according to the props of the ProgreeStepper Component

--- src/pages/Database/Database.js
+++ src/pages/Database/Database.js
@@ -2,12 +2,20 @@ import Header from "../../components/Header/Header";
 import BasicCard from "../../components/BasicCard/BasicCard";
 import ProgressStepper from "../../components/ProgressStepper/ProgressStepper";
 
+const steps = ['Step 1', 'Step 2', 'Step 3'];
+const stepDescription = ['Description 1', 'Description 2', 'Description 3'];
+
 function Database() {
     return (
         <div style={{ marginLeft:320}}>
             <Header title='Database'/>
             <BasicCard 
-                content={<ProgressStepper />}
+                content={
+                    <ProgressStepper 
+                        steps={steps}
+                        stepDescription={stepDescription}
+                    />
+                }
             />
         </div>
     )
