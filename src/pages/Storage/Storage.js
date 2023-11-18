dc40d1bcb076e056da729d77e68728d5fd1a54ce
rasha-massoud <rashamassoud1998@gmail.com>
2023-11-18 22:28:26 +0200

    Updated the Storage Page

--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,65 +1,30 @@
 import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import BasicSnapBar from "../../components/BasicSnapBar/BasicSnapBar";
-import CommonButton from "../../components/CommonButton/CommonButton";
-import { storageStyles } from './styles';
-import Loading from "../../components/Loading/Loading";
+import BasicCard from "../../components/BasicCard/BasicCard";
+import UserTable from "../../components/UserTable/UserTable";
 
 function Storage() {
     const [open, setOpen] = useState(false);
-    const [loading, setLoading] = useState(true)
-
-    const severity = "success";
-    const message = `This is a ${severity} message`;
-    const ButtonText = `Open the ${severity} snapbar`;
-
-    useEffect(()=> {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 3000);
-        return () => clearTimeout(timer);
-    },[]);
-
-    const handleClick = () => {
-        setOpen(true);
-    };
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
-            return;
+          return;
         }
-
         setOpen(false);
     };
 
     return (
         <div style={{ marginLeft:320}}>
             <Header title='Storage'/>
-            { loading ?
-                <Loading sx={storageStyles.button}>
-                    <CommonButton 
-                        sx={storageStyles.button}
-                        variant="contained" 
-                        onClick={handleClick}
-                    >
-                        {ButtonText}
-                    </CommonButton>
-                </Loading>
-                :
-                <CommonButton 
-                        sx={storageStyles.button}
-                        variant="contained" 
-                        onClick={handleClick}
-                    >
-                        {ButtonText}
-                </CommonButton>
-            }
-
-            <BasicSnapBar 
+            <BasicCard 
+                content={<UserTable onError={() => setOpen(true)} />}
+            />
+            <BasicSnapBar
                 open={open}
+                severity="error"
+                message="Data couldn't be fetched"
                 onClose={handleClose}
-                severity={severity}
-                message={message}
             />
         </div>
     )
--- src/pages/Storage/styles.js
+++
@@ -1,7 +0,0 @@
-export const storageStyles = {
-    button: {
-        marginTop: 10,
-        margin: "2% 2%",
-        width: "96%"
-    }
-}
